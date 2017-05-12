593bec9c8ab5ca1f48335a1f87d20a4adf520f9c
Christopher Groskopf <chrisgroskopf@gmail.com>
2017-05-12 11:24:26 -0500

    Uncommited changes.

--- scene.py
+++ scene.py
@@ -230,6 +230,60 @@ class Scene(object):
         print(cmd)
         run(cmd)
 
+    def hist_match(self, source, template):
+        """
+        Adjust the pixel values of a grayscale image such that its histogram
+        matches that of a target image
+
+        Arguments:
+        -----------
+            source: np.ndarray
+                Image to transform; the histogram is computed over the flattened
+                array
+            template: np.ndarray
+                Template image; can have different dimensions to source
+        Returns:
+        -----------
+            matched: np.ndarray
+                The transformed output image
+        """
+
+        oldshape = source.shape
+        source = source.ravel()
+        template = template.ravel()
+
+        # get the set of unique pixel values and their corresponding indices and
+        # counts
+        s_values, bin_idx, s_counts = np.unique(source, return_inverse=True,
+                                                return_counts=True)
+        t_values, t_counts = np.unique(template, return_counts=True)
+
+        s_counts[0] = 0
+        print(t_counts)
+
+        # take the cumsum of the counts and normalize by the number of pixels to
+        # get the empirical cumulative distribution functions for the source and
+        # template images (maps pixel value --> quantile)
+        s_quantiles = np.cumsum(s_counts)
+        s_quantiles = (255 * s_quantiles / s_quantiles[-1]).astype(np.ubyte) #normalize
+        t_quantiles = np.cumsum(t_counts).astype(np.float64)
+        t_quantiles = (255 * t_quantiles / t_quantiles[-1]).astype(np.ubyte) #normalize
+
+        # source_cdf, source_bin_centers = exposure.cumulative_distribution(source)
+        # template_cdf, template_bin_centers = exposure.cumulative_distribution(template)
+        # out = np.interp(image.flat, bin_centers, cdf)
+        # out = np.interp(source.flat, template_bin_centers, template_cdf)
+
+        # # interpolate linearly to find the pixel values in the template image
+        # # that correspond most closely to the quantiles in the source image
+        interp_t_values = np.interp(s_quantiles, t_quantiles, t_values).astype(np.ubyte)
+
+        print interp_t_values
+
+        return interp_t_values[bin_idx].reshape(oldshape)
+
+        # return out.reshape(oldshape)
+
     def color_correct(self):
         if not self.crop_file_exists:
             raise IOError('Crop file does not exist')
@@ -239,43 +293,40 @@ class Scene(object):
 
         print('Equalizing histograms')
 
-        from skimage.color.adapt_rgb import adapt_rgb, each_channel
-
-        @adapt_rgb(each_channel)
-        def correct(image):
-            image = exposure.adjust_gamma(image, 1.25)
-            # p2, p98 = np.percentile(rolled, (2, 99))
-            return
-
         with rasterio.drivers():
+            with rasterio.open('correct.tif') as f:
+                template = np.rollaxis(np.rollaxis(f.read(), 1), 2, 1)
+
             with rasterio.open('%s/crop.tif' % self.output_dir) as f:
                 data = f.read()
                 profile = f.profile
 
             rolled = np.rollaxis(np.rollaxis(data, 1), 2, 1)
 
-            new_bands = []
+            # new_bands = []
+            #
+            # for b, band in enumerate(rolled.T):
+            #     # R
+            #     if b == 0:
+            #         in_range = (28, 130)
+            #     # G
+            #     elif b == 1:
+            #         in_range = (41, 105)
+            #     # B
+            #     elif b == 2:
+            #         in_range = (58, 120)
+            #
+            #     new_bands.append(
+            #         exposure.rescale_intensity(band, in_range=in_range)
+            #     )
+            #
+            # rescaled = np.array(new_bands).T
 
-            for b, band in enumerate(rolled.T):
-                # R
-                if b == 0:
-                    in_range = (28, 130)
-                # G
-                elif b == 1:
-                    in_range = (41, 105)
-                # B
-                elif b == 2:
-                    in_range = (58, 120)
-
-                new_bands.append(
-                    exposure.rescale_intensity(band, in_range=in_range)
-                )
-
-            rescaled = np.array(new_bands).T
+            # rescaled = img_as_ubyte(self.hist_match(rolled, template))
 
             # rolled = exposure.adjust_gamma(rolled, 1.25)
-            # rescaled = exposure.rescale_intensity(rolled, in_range=(p2, p98))
-            # rescaled = img_as_ubyte(exposure.equalize_adapthist(rolled))
+            rescaled = exposure.rescale_intensity(rolled, in_range=(1, 255))
+            rescaled = img_as_ubyte(exposure.equalize_adapthist(rolled))
             # rescaled = correct(rolled)
             # rescaled = img_as_ubyte(exposure.adjust_sigmoid(rolled, 0.25, 10))
 
